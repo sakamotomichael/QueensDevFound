d90722041ac27ecef582211b04dd7d001cee040b
Leonard Ha <leonard.ha@gmail.com>
2016-07-27 20:02:31 +0800

    hello# Please enter the commit message for your changes. Lines starting

--- public_html/Queens.php
+++ public_html/Queens.php
@@ -1,153 +1,254 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset = "UTF-8">
+<meta http-equiv="X-UA-Compatible" content = "IE=edge">
+<!-- Makes sure that it fits the page regardless of screen size-->
+<meta name = "viewport" content = "width = device-width, initial-scale = 1">
 <title>Developers' Foundation - Queen's Chapter</title>
 
-<link rel="stylesheet" type="text/css" href="QueensCSS.css" />
+<link rel = "stylesheet" type = "text/css" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
-<script src = "jquery-3.1.0.js"></script>
+<script src = "jquery-3.0.0.min.js"></script>
 
 <link rel = "icon" type = "image/ico" href = "DFFavicon.ico">
+
+<style>
+
+@font-face {
+	font-family: 'JosefinSans Regular';
+	src: url('JosefinSans-Regular.ttf') format('truetype');
+}
+
+.WWDtext{
+	font-size:150%;
+	font-family: "JosefinSans Regular";
+}
+
+.button{
+	background-color: #F7F7F7;
+	border: none;
+	color: #63768D;
+	padding: 15px 32px;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	font-size:150%;
+	font-family: "JosefinSans Regular";
+	border-radius: 2px;
+	margin-top:10px;
+}
+#titlemsg{
+	font-size:8vh;
+	color:white;
+	text-align: center;
+	font-family: "JosefinSans Regular";
+	position:absolute;
+	line-height:200px;
+	top:48%;
+	left:0;
+	right:0;
+}
+
+</style>
+
 </head>
 
 <body>
+<div class = "container-fluid">
 
-<div id = "navigationBar">
+<!--
+	<div class = "row">
+    <div class = "col-lg-10 col-md-offset-1">
+    <div class = "row">
+    <div class = "col-lg-6" style = "font-size:200%;font-family:'JosefinSans Regular'; padding-top:20px;padding-bottom:20px;">
+    Developer's Foundation - Queen's Chapter
+    </div>
+    <div class = "col-lg-6" style = "padding-bottom:10px;">
+    <button class = "button" id = "button1">About</button>
+	<button class = "button" id = "button2">Our Services</button>
+    <button class = "button" id = "button3">Portfolio</button>
+	<button class = "button" id = "button4">Contact</button>
+    </div>
+    </div>
+    </div>
+    </div>
+    -->
+    <div class = "row" style = "background-image:url(QueensTitleImage.jpg);background-repeat: no-repeat;
+	background-size: cover; height:100vh;"> <!--Navbar and titleImage row-->
+    
+    	<div class = "col-12-lg container-fluid">
+    
+    		<div class = "row" style = "background-color:white;"> <!--Navbar-->
+    			<div class = "col-lg-10 col-md-offset-1">
+    				<div class = "row">
+    					<div class = "col-lg-6" style = "font-size:200%;font-family:'JosefinSans Regular'; padding-top:20px;padding-bottom:20px;">
+    Developer's Foundation - Queen's Chapter
+    					</div>
+                    
+    					<div class = "col-lg-6" style = "padding-bottom:10px;">
+    <button class = "button" id = "button1">About</button>
+	<button class = "button" id = "button2">Our Services</button>
+    <button class = "button" id = "button3">Portfolio</button>
+	<button class = "button" id = "button4">Contact</button>
+    					</div>
+    				</div>
+    			</div>
+    		</div> <!--Close Navbar-->
+    
+    	
+        	<div = "row">
+    			<div = "col-lg-12">
+    				<div id = "titlemsg">
+						Developer's Foundation
+    				</div>
+    			</div>
+    		</div>
+        </div>
+	</div> <!-- Close navbar and titleScreen-->
 
-<div style = "width:70%; margin-left:auto;margin-right:auto;">
-
-
-<div id = "devFoundation">
-Developer's Foundation
-</div>
-
-
-<div id = "buttons">
-<button class = "button" id = "button2">About</button>
-<button class = "button" id = "button3">Our Services</button>
-<button class = "button" id = "button4">Contact</button>
-</div>
-
-<div style="clear: both;"></div>
-</div>
-</div>
-
-
-<div class = "Section1">
-<h1 id = "title1">Developers' Foundation</h1>
-<p id = "message">Queen's Chapter</p>
-</div>
-
-<div class = "Section2">
-<div style="width: 100%;">
-<div id = "content2">
-<h1 id = "title2">About</h1>
-
-<div style = "float:left; width:50%;">
-<img src="Granthall.JPG" height = "50%" width = "80%" style = "border:2px solid black; border-radius: 5px;">
-</div>
-
-<div style = "float:left; width:50%;">
-<div style = "padding-top:5vh;">
-<p>
-We're a team of students and aspiring web designers which serve as a subteam for Developers' Foundation. 
+  
+    
+    <div class = "row" style = "padding-bottom:50px;"> <!--About section-->
+		<div class = "col-lg-10 col-md-offset-1">
+        	<div class = "row"><!--About row-->
+            	<div class = "col-lg-10">
+            		<div class = "col-lg-8 col-md-offset-2" style = "padding-bottom:20px; padding-top:20px;">
+            			<h1 style = "font-size:400%; font-family:'JosefinSans Regular';">About</h1>	
+                	</div>
+            	</div>
+            </div><!--Close about row-->
+            
+            <div class = "row"> <!--Picture and description row-->
+            	<div class = "col-lg-6 text-center"> <!--Image column-->
+ 					<img src="Granthall.JPG" style = "border:2px solid black; border-radius: 5px; margin-top: 10px; margin-bottom: 10px;" class = "img-responsive center-block">
+				</div> <!--Close Image column-->
+            
+            	<div class = "col-lg-6"> <!-- Description column-->
+            		<div class = "col-lg-10" style = "font-size:150%; font-family:'JosefinSans Regular'; text-align:justify;">
+            			<h2><b>Every company deserves to be recognized</b></h2>
+            			<br />
+        				<p> We're a team of students and aspiring web designers which serve as a subteam for Developers' Foundation. 			</p>
+						<p> In charge of operations at Queen's University and the city of Kingston, we primarily seek out NPOs in the city and assist them in establishing an online presence.
+						</p>	
+                        
+					</div>
+            	</div> <!-- Close Description column-->
+            </div>     
+        </div>
+	</div><!--Close about section-->
+    
+    <div class = "row" style = "padding-bottom:40px; background-color:#fbfbfb;"> <!--WhatWeDo section-->
+    
+		<div class = "col-lg-10 col-md-offset-1"> <!--column for centering -->
+        
+        	<div class = "row"> <!--WhatWeDo title-->
+        		<div style = "text-align:center;">
+            		<h1 style = "font-size:400%; font-family:'JosefinSans Regular';">What we do</h1>	
+            	</div>
+            </div><!--Close WhatWeDo title-->
+            
+            <div class = "row" style = "padding-bottom:20px"> <!--3 panels section-->
+      
+            	<div class = "col-lg-4"><!--EstablishAConnection Section-->
+            		<div class = "row text-center">
+            			<h2 style = "font-family:'JosefinSans Regular';">Establish a connection with us</h2>
+            		</div>
+            			<div class = "text-center" style = "padding-top:20px;padding-bottom:20px;">
+            				<span style="font-size:10em; color:#212A8F;" class = "glyphicon glyphicon-link"></span>
+            			</div>
+  			
+  					<div class = "WWDtext row">
+            			<div class = "col-lg-10 col-md-offset-1" style = "text-align:justify;">
+ 							<p> Are you an NPO in Kingston or a club with NPO connections looking to get online? </p>
+							<p> We're here to help you for little to no cost. Contact us and we can get you started. You'll be directed to this team. </p>
+						</div>
+           			 </div>
+				</div> <!-- Close EstablishAConnection Section-->
+            
+           		<div class = "col-lg-4"> <!--WeMakeYourWebsite Section-->
+            		<div class = "row text-center">
+            			<h2 style = "font-family:'JosefinSans Regular';">We create your website</h2>
+            		</div>
+            			<div class = "text-center" style = "padding-top:20px;padding-bottom:20px;">
+            				<span style="font-size:10em; color:#F9FF8C;" class = "glyphicon glyphicon-file"></span>
+            			</div>
+            
+            		<div class = "WWDtext row">
+            			<div class = "col-lg-10 col-md-offset-1" style = "text-align:justify;">
+        					<p> Our team is comprised of five aspiring web developers and business students. </p>
+							<p> We'll be in constant contact with you to ensure that you have the perfect website that your NPO deserves. </p>
+						</div>
+            		</div>
+         		</div><!--Close WeMakeYourWebsite Section-->
+            
+            
+            	<div class = "col-lg-4"> <!-- YourSiteWillBeHosted section-->
+            		<div class = "row text-center">
+            			<h2 style = "font-family:'JosefinSans Regular';">Your site will be hosted</h2>
+            		</div>
+            			<div class = "text-center" style = "padding-top:20px;padding-bottom:20px;">
+            				<span style="font-size:10em; color:#D60909;" class = "glyphicon glyphicon-globe"></span>
+            			</div>
+                        
+            		<div class = "WWDtext row">
+            			<div class = "col-lg-10 col-md-offset-1" style = "text-align:justify;">
+            				<p> The website we create for you will be hosted using Heroku, with no initial or monthly costs.
 </p>
-<p>
-In charge of operations at Queen's University and the city of Kingston, we primarily seek out NPOs in the city and assist them in establishing an online presence.
-</p>
-</div>
-</div>
-
-<div style="clear: both;"></div>
-
-</div>
-</div>
-</div>
-
-<div class = "Section3">
-<h1 id = "title3">Our Services</h1>
-<div style="width: 100%;">
-<div id = "content3">
-
-<div style = "float:left; width:33%;margin-right:0.5%">
-<h2 style = "text-align: center;">Contact us</h2>
-<br />
-<div style="height:10vh; width:30%;display: block; margin-left:auto; margin-right:auto;">
-
-<img src="Contact.png" height = "100%" width = "100%" style = "display: block; margin-left:auto; margin-right:auto;"/>
-
-</div>
-<div style = "width:80%;display: block; margin-left:auto; margin-right:auto;">
-<p>
-Are you an NPO in Kingston or a club with NPO connections looking to get online? 
-</p>
-<p>
-We're here to help you for little to no cost. Contact us and we can get you started. You'll be directed to this team.
-</p>
-</div>
-</div>
-
-<div style = "float:left; width:33%;margin-right: 0.5%;">
-<h2 style = "text-align: center;">We'll create your website</h2>
-<br />
-
-<div style="height:10vh; width:30%;display: block; margin-left:auto; margin-right:auto;">
-<img src="Create.png" height = "100%" width = "100%" style = "display: block; margin-left:auto; margin-right:auto;"/>
-</div>
-
-<div style = "width:80%;display: block; margin-left:auto; margin-right:auto;">
-<p>
-Our team is comprised of four aspiring web developers and business students. 
-</p>
-<p>
-We'll be in constant contact with you to ensure that you have the perfect website for your company. 
-</p>
-</div>
-</div>
-
-<div style = "float:left; width:33%;">
-<h2 style = "text-align: center;">Hosting your site</h2>
-<br />
-
-<div style="height:10vh; width:30%;display: block; margin-left:auto; margin-right:auto;">
-<img src="Host.png" height = "100%" width = "100%" style = "display: block; margin-left:auto; margin-right:auto;"/>
-</div>
+							<p> Your site will be on the internet for as long as it needs to be.</p> </div>
+            			</div>
+            		</div>
+            	</div> <!--Close YourSiteWillBeHosted section-->
+                
+                  
+        	</div> <!--Close column for centering -->
+		</div><!--Close WhatWeDo section-->
+    
+    
+    <div class = "row" style = "font-family:'JosefinSans Regular'; padding-bottom:40px;"><!--Portfolio section-->
+    	<div class = "col-lg-8 col-md-offset-2">
+        
+        	<div class = "row"> <!--Portfolio title-->
+            	<div style = "text-align:center;">
+            		<h1 style = "font-size:400%; font-family:'JosefinSans Regular';">Portfolio</h1>	
+            	</div>
+			</div><!--Close Portfolio title-->
+            
+            <div class = "row"> <!--Portfolio info-->
+            	<div class = "col-lg-12">
+                	Fill in portfolio here
+                </div>
+            </div> <!--Close Portfolio info-->
+      
+        </div>
+    </div> <!--Close portfolio section-->
+    
+    <div class = "row" style = "font-family:'JosefinSans Regular'; padding-bottom:40px;"> <!--ContactUS Section-->
+    
+		<div class = "col-lg-8 col-md-offset-2">
+        
+        	<h1 style = "font-size:350%; padding-bottom:40px;">Are you interested? Let's initiate a connection.</h1>
+            
+        	<p style = "font-size:200%">Visit the <a href="https://developersfoundation.ca/">main Developers' Foundation website</a> for more information and to send a message to the main team. You will be directed to us if you are situated in the Kingston area. </p>
+        </div>
+        
+	</div> <!--Close ContactUS Section-->
+    
+    <div class = "row" style = "font-family:'JosefinSans Regular';padding-bottom:40px; background-color:#E6F7FC"> <!--Footer-->
+		<div class = "col-lg-12">
+        	<div class = "col-lg-10 col-md-offset-1" style = "font-size:150%; padding-top:40px;">
+        		Copyright © 2016. Developers' Foundation. All rights reserved.
+        	</div>
+        </div>
+	</div><!--Close Footer-->
+           
+</div> <!-- Close main container-->
 
 
-<div style = "width:80%;display: block; margin-left:auto; margin-right:auto;">
-<p>
-The website we create for you will be hosted on the Developers' Foundation servers.
-</p>
-</div>
-</div>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
-<div style="clear: both;"></div>
-
-</div>
-</div>
-</div>
-
-<div class = "Section4">
-<div style="width: 100%;">
-<div id = "content4">
-<h1 id = "title4">Contact</h1>
-<div style = "margin-bottom:5%;">
-<p>
-Visit the <a href="https://developersfoundation.ca/">main Developers' Foundation website</a> for more information. You will be directed to us if you are situated in the Kingston area.
-</p>
-</div>
-<button class = "button" id = "backToTop">Back to top</button>
-</div>
-</div>
-</div>
-
-<div style = "height: 10vh; width:100%; background-color:#0A0E24; color:white">
-<div id = "footer">
-Copyright © 2016. Developers' Foundation. All rights reserved.
-</div>
-</div>
-
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 </body>
 </html>
@@ -167,12 +268,6 @@ Copyright © 2016. Developers' Foundation. All rights reserved.
 		setTimeout(function(){
 			$('#devFoundation').css({visibility: "visible"}).fadeIn(1500);
 		},1000); 
-		
-		
-		
-		
-		
-		
 		
 		$(".button").hover(function(){
 			$(this).css("background-color","#D2D6D9");
