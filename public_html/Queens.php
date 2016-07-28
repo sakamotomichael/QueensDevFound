07280601fc35f031bff5ed0f7fc4672fa3580900
Leonard Ha <leonard.ha@gmail.com>
2016-07-28 21:03:18 +0800

    hi

--- public_html/Queens.php
+++ public_html/Queens.php
@@ -2,16 +2,16 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta charset = "UTF-8">
-<meta http-equiv="X-UA-Compatible" content = "IE=edge">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <!-- Makes sure that it fits the page regardless of screen size-->
-<meta name = "viewport" content = "width = device-width, initial-scale = 1">
+<meta name="viewport" content = "width=device-width, initial-scale = 1">
 <title>Developers' Foundation - Queen's Chapter</title>
 
-<link rel = "stylesheet" type = "text/css" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link rel = "stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
-<script src = "jquery-3.0.0.min.js"></script>
+<script src="jquery-3.0.0.min.js"></script>
 
-<link rel = "icon" type = "image/ico" href = "DFFavicon.ico">
+<link rel="icon" type="image/ico" href="DFFavicon.ico">
 
 <style>
 
@@ -39,13 +39,13 @@
 	margin-top:10px;
 }
 #titlemsg{
-	font-size:8vh;
+	font-size:5em;
 	color:white;
 	text-align: center;
 	font-family: "JosefinSans Regular";
 	position:absolute;
-	line-height:200px;
-	top:48%;
+	padding-top:20%;
+	padding-bottom:20%;
 	left:0;
 	right:0;
 }
@@ -55,7 +55,7 @@
 </head>
 
 <body>
-<div class = "container-fluid">
+<div class="container-fluid">
 
 <!--
 	<div class = "row">
@@ -74,32 +74,32 @@
     </div>
     </div>
     -->
-    <div class = "row" style = "background-image:url(QueensTitleImage.jpg);background-repeat: no-repeat;
-	background-size: cover; height:100vh;"> <!--Navbar and titleImage row-->
+    <div class="row" style="background-image:url(QueensTitleImage.jpg);background-repeat: no-repeat;
+	background-size:cover;min-height:600px;height:100vh;"> <!--Navbar and titleImage row-->
     
-    	<div class = "col-12-lg container-fluid">
+    	<div class="col-12-lg container-fluid">
     
-    		<div class = "row" style = "background-color:white;"> <!--Navbar-->
-    			<div class = "col-lg-10 col-md-offset-1">
-    				<div class = "row">
-    					<div class = "col-lg-6" style = "font-size:200%;font-family:'JosefinSans Regular'; padding-top:20px;padding-bottom:20px;">
+    		<div class= "row" style="background-color:white;"> <!--Navbar-->
+    			<div class="col-lg-10 col-md-offset-1">
+    				<div class="row">
+    					<div class="col-lg-6" style="font-size:200%;font-family:'JosefinSans Regular'; padding-top:20px;padding-bottom:20px;">
     Developer's Foundation - Queen's Chapter
     					</div>
                     
-    					<div class = "col-lg-6" style = "padding-bottom:10px;">
-    <button class = "button" id = "button1">About</button>
-	<button class = "button" id = "button2">Our Services</button>
-    <button class = "button" id = "button3">Portfolio</button>
-	<button class = "button" id = "button4">Contact</button>
+    					<div class="col-lg-6" style="padding-bottom:10px;">
+    <button class="button" id="button1">About</button>
+	<button class="button" id="button2">Our Services</button>
+    <button class="button" id="button3">Portfolio</button>
+	<button class="button" id="button4">Contact</button>
     					</div>
     				</div>
     			</div>
     		</div> <!--Close Navbar-->
     
     	
-        	<div = "row">
-    			<div = "col-lg-12">
-    				<div id = "titlemsg">
+        	<div class="row">
+    			<div class="col-lg-12">
+    				<div id="titlemsg">
 						Developer's Foundation
     				</div>
     			</div>
@@ -109,23 +109,23 @@
 
   
     
-    <div class = "row" style = "padding-bottom:50px;"> <!--About section-->
-		<div class = "col-lg-10 col-md-offset-1">
-        	<div class = "row"><!--About row-->
-            	<div class = "col-lg-10">
-            		<div class = "col-lg-8 col-md-offset-2" style = "padding-bottom:20px; padding-top:20px;">
-            			<h1 style = "font-size:400%; font-family:'JosefinSans Regular';">About</h1>	
+    <div class="row About" style="padding-bottom:50px;"> <!--About section-->
+		<div class="col-lg-10 col-md-offset-1">
+        	<div class="row"><!--About row-->
+            	<div class="col-lg-10">
+            		<div class="col-lg-8 col-md-offset-2" style="padding-bottom:20px; padding-top:20px;">
+            			<h1 style="font-size:400%; font-family:'JosefinSans Regular';">About</h1>	
                 	</div>
             	</div>
             </div><!--Close about row-->
             
-            <div class = "row"> <!--Picture and description row-->
-            	<div class = "col-lg-6 text-center"> <!--Image column-->
- 					<img src="Granthall.JPG" style = "border:2px solid black; border-radius: 5px; margin-top: 10px; margin-bottom: 10px;" class = "img-responsive center-block">
+            <div class="row"> <!--Picture and description row-->
+            	<div class="col-lg-6 text-center"> <!--Image column-->
+ 					<img src="Granthall.JPG" style="border:2px solid black; border-radius: 5px; margin-top: 10px; margin-bottom: 10px;" class="img-responsive center-block">
 				</div> <!--Close Image column-->
             
-            	<div class = "col-lg-6"> <!-- Description column-->
-            		<div class = "col-lg-10" style = "font-size:150%; font-family:'JosefinSans Regular'; text-align:justify;">
+            	<div class="col-lg-6"> <!-- Description column-->
+            		<div class="col-lg-10" style="font-size:150%; font-family:'JosefinSans Regular'; text-align:justify;">
             			<h2><b>Every company deserves to be recognized</b></h2>
             			<br />
         				<p> We're a team of students and aspiring web designers which serve as a subteam for Developers' Foundation. 			</p>
@@ -138,44 +138,44 @@
         </div>
 	</div><!--Close about section-->
     
-    <div class = "row" style = "padding-bottom:40px; background-color:#fbfbfb;"> <!--WhatWeDo section-->
+    <div class="row WhatWeDo" style="padding-bottom:40px; background-color:#fbfbfb;"> <!--WhatWeDo section-->
     
-		<div class = "col-lg-10 col-md-offset-1"> <!--column for centering -->
+		<div class="col-lg-10 col-md-offset-1"> <!--column for centering -->
         
-        	<div class = "row"> <!--WhatWeDo title-->
-        		<div style = "text-align:center;">
-            		<h1 style = "font-size:400%; font-family:'JosefinSans Regular';">What we do</h1>	
+        	<div class="row"> <!--WhatWeDo title-->
+        		<div style="text-align:center;">
+            		<h1 style="font-size:400%; font-family:'JosefinSans Regular';">What we do</h1>	
             	</div>
             </div><!--Close WhatWeDo title-->
             
-            <div class = "row" style = "padding-bottom:20px"> <!--3 panels section-->
+            <div class="row" style="padding-bottom:20px"> <!--3 panels section-->
       
-            	<div class = "col-lg-4"><!--EstablishAConnection Section-->
-            		<div class = "row text-center">
-            			<h2 style = "font-family:'JosefinSans Regular';">Establish a connection with us</h2>
+            	<div class="col-lg-4"><!--EstablishAConnection Section-->
+            		<div class="row text-center">
+            			<h2 style="font-family:'JosefinSans Regular';">Establish a connection with us</h2>
             		</div>
-            			<div class = "text-center" style = "padding-top:20px;padding-bottom:20px;">
-            				<span style="font-size:10em; color:#212A8F;" class = "glyphicon glyphicon-link"></span>
+            			<div class="text-center" style="padding-top:20px;padding-bottom:20px;">
+            				<span style="font-size:10em; color:#212A8F;" class="glyphicon glyphicon-link"></span>
             			</div>
   			
-  					<div class = "WWDtext row">
-            			<div class = "col-lg-10 col-md-offset-1" style = "text-align:justify;">
+  					<div class="WWDtext row">
+            			<div class="col-lg-10 col-md-offset-1" style="text-align:justify;">
  							<p> Are you an NPO in Kingston or a club with NPO connections looking to get online? </p>
 							<p> We're here to help you for little to no cost. Contact us and we can get you started. You'll be directed to this team. </p>
 						</div>
            			 </div>
 				</div> <!-- Close EstablishAConnection Section-->
             
-           		<div class = "col-lg-4"> <!--WeMakeYourWebsite Section-->
-            		<div class = "row text-center">
-            			<h2 style = "font-family:'JosefinSans Regular';">We create your website</h2>
+           		<div class="col-lg-4"> <!--WeMakeYourWebsite Section-->
+            		<div class="row text-center">
+            			<h2 style="font-family:'JosefinSans Regular';">We create your website</h2>
             		</div>
-            			<div class = "text-center" style = "padding-top:20px;padding-bottom:20px;">
-            				<span style="font-size:10em; color:#F9FF8C;" class = "glyphicon glyphicon-file"></span>
+            			<div class="text-center" style="padding-top:20px;padding-bottom:20px;">
+            				<span style="font-size:10em; color:#F9FF8C;" class="glyphicon glyphicon-file"></span>
             			</div>
             
-            		<div class = "WWDtext row">
-            			<div class = "col-lg-10 col-md-offset-1" style = "text-align:justify;">
+            		<div class="WWDtext row">
+            			<div class="col-lg-10 col-md-offset-1" style="text-align:justify;">
         					<p> Our team is comprised of five aspiring web developers and business students. </p>
 							<p> We'll be in constant contact with you to ensure that you have the perfect website that your NPO deserves. </p>
 						</div>
@@ -183,16 +183,16 @@
          		</div><!--Close WeMakeYourWebsite Section-->
             
             
-            	<div class = "col-lg-4"> <!-- YourSiteWillBeHosted section-->
-            		<div class = "row text-center">
-            			<h2 style = "font-family:'JosefinSans Regular';">Your site will be hosted</h2>
+            	<div class="col-lg-4"> <!-- YourSiteWillBeHosted section-->
+            		<div class="row text-center">
+            			<h2 style="font-family:'JosefinSans Regular';">Your site will be hosted</h2>
             		</div>
-            			<div class = "text-center" style = "padding-top:20px;padding-bottom:20px;">
-            				<span style="font-size:10em; color:#D60909;" class = "glyphicon glyphicon-globe"></span>
+            			<div class="text-center" style="padding-top:20px;padding-bottom:20px;">
+            				<span style="font-size:10em; color:#D60909;" class="glyphicon glyphicon-globe"></span>
             			</div>
                         
-            		<div class = "WWDtext row">
-            			<div class = "col-lg-10 col-md-offset-1" style = "text-align:justify;">
+            		<div class="WWDtext row">
+            			<div class="col-lg-10 col-md-offset-1" style="text-align:justify;">
             				<p> The website we create for you will be hosted using Heroku, with no initial or monthly costs.
 </p>
 							<p> Your site will be on the internet for as long as it needs to be.</p> </div>
@@ -205,17 +205,17 @@
 		</div><!--Close WhatWeDo section-->
     
     
-    <div class = "row" style = "font-family:'JosefinSans Regular'; padding-bottom:40px;"><!--Portfolio section-->
-    	<div class = "col-lg-8 col-md-offset-2">
+    <div class="row Portfolio" style="font-family:'JosefinSans Regular'; padding-bottom:40px;"><!--Portfolio section-->
+    	<div class="col-lg-8 col-md-offset-2">
         
-        	<div class = "row"> <!--Portfolio title-->
-            	<div style = "text-align:center;">
-            		<h1 style = "font-size:400%; font-family:'JosefinSans Regular';">Portfolio</h1>	
+        	<div class="row"> <!--Portfolio title-->
+            	<div style="text-align:center;">
+            		<h1 style="font-size:400%; font-family:'JosefinSans Regular';">Portfolio</h1>	
             	</div>
 			</div><!--Close Portfolio title-->
             
-            <div class = "row"> <!--Portfolio info-->
-            	<div class = "col-lg-12">
+            <div class="row"> <!--Portfolio info-->
+            	<div class="col-lg-12">
                 	Fill in portfolio here
                 </div>
             </div> <!--Close Portfolio info-->
@@ -223,20 +223,20 @@
         </div>
     </div> <!--Close portfolio section-->
     
-    <div class = "row" style = "font-family:'JosefinSans Regular'; padding-bottom:40px;"> <!--ContactUS Section-->
+    <div class="row Contact" style="font-family:'JosefinSans Regular'; padding-bottom:40px;"> <!--ContactUS Section-->
     
-		<div class = "col-lg-8 col-md-offset-2">
+		<div class="col-lg-8 col-md-offset-2">
         
-        	<h1 style = "font-size:350%; padding-bottom:40px;">Are you interested? Let's initiate a connection.</h1>
+        	<h1 style="font-size:350%; padding-bottom:40px;">Are you interested? Let's initiate a connection.</h1>
             
-        	<p style = "font-size:200%">Visit the <a href="https://developersfoundation.ca/">main Developers' Foundation website</a> for more information and to send a message to the main team. You will be directed to us if you are situated in the Kingston area. </p>
+        	<p style="font-size:200%">Visit the <a href="https://developersfoundation.ca/">main Developers' Foundation website</a> for more information and to send a message to the main team. You will be directed to us if you are situated in the Kingston area. </p>
         </div>
         
 	</div> <!--Close ContactUS Section-->
     
-    <div class = "row" style = "font-family:'JosefinSans Regular';padding-bottom:40px; background-color:#E6F7FC"> <!--Footer-->
-		<div class = "col-lg-12">
-        	<div class = "col-lg-10 col-md-offset-1" style = "font-size:150%; padding-top:40px;">
+    <div class="row" style="font-family:'JosefinSans Regular';padding-bottom:20px; background-color:black;"> <!--Footer-->
+		<div class="col-lg-12">
+        	<div class="col-lg-10 col-md-offset-1" style="font-size:150%; padding-top:20px;color:white;">
         		Copyright © 2016. Developers' Foundation. All rights reserved.
         	</div>
         </div>
@@ -278,28 +278,28 @@
 		
 		<!-- SCROLL TO -->
 		
-		$("#button2").click(function() {
+		$("#button1").click(function() {
 			$('html, body').animate({
-				scrollTop: $(".Section2").offset().top
+				scrollTop: $(".About").offset().top
 			}, 1500);
 		});
+		
+		$("#button2").click(function() {
+			$('html, body').animate({
+				scrollTop: $(".WhatWeDo").offset().top
+			}, 1500);
+		});
+		
 		
 		$("#button3").click(function() {
 			$('html, body').animate({
-				scrollTop: $(".Section3").offset().top
+				scrollTop: $(".Portfolio").offset().top
 			}, 1500);
 		});
-		
 		
 		$("#button4").click(function() {
 			$('html, body').animate({
-				scrollTop: $(".Section4").offset().top
-			}, 1500);
-		});
-		
-		$("#backToTop").click(function() {
-			$('html, body').animate({
-				scrollTop: $("#navigationBar").offset().top
+				scrollTop: $(".Contact").offset().top
 			}, 1500);
 		});
 		
