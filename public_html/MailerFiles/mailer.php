b0fef6a7b339eaa312c47970ede2eb128afcaa36
Leonard Ha <leonard.ha@gmail.com>
2016-08-06 21:47:14 +0800

    Hi

--- public_html/MailerFiles/mailer.php
+++ public_html/MailerFiles/mailer.php
@@ -12,9 +12,8 @@ if($_SERVER['REQUEST_METHOD'] === 'GET'){
 }
 
 require '../../vendor/autoload.php'; // MAKE SURE THIS POINTS TO YOUR COMPOSER VENDOR FOLDER
-//$getPost = (array)json_decode(file_get_contents('php://input'));
+$getPost = (array)json_decode(file_get_contents('php://input'));
 
-$getPost = $_POST; 
 
 $sendgrid = new SendGrid('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');
 $email = new SendGrid\Email();
